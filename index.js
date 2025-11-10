8b21c816da6fe31a50ac7135020011511b34a67d
Amin-ur-Rahman <aminurar679799@gmail.com>
2025-11-10 22:35:17 +0600

    post api for insert review added, get api for all reviews added

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const cors = require("cors");
 
 const port = 5000;
+
+// vTavoOIHVG9gPnwF;
+// local-food-lovers-project;
+
+const uri =
+  "mongodb+srv://local-food-lovers-project:vTavoOIHVG9gPnwF@cluster0.ix21m2z.mongodb.net/?appName=Cluster0";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 //middlewares
 app.use(cors());
@@ -11,9 +25,44 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("server connected!");
 });
-app.get("/hommy", (req, res) => {
-  res.send("hommies connected!");
-});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("local-food-lovers-db");
+    const reviewCollection = db.collection("food_reviews");
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    app.post("/insert-new-review", async (req, res) => {
+      const query = req.body;
+
+      try {
+        const result = await reviewCollection.insertOne(query);
+        res.send(result);
+        console.log("review added successfully", result);
+      } catch (error) {
+        console.log("request error!", error);
+      }
+    });
+
+    app.get("/reviews", async (req, res) => {
+      try {
+        const cursor = reviewCollection.find();
+        const data = await cursor.toArray();
+        res.send(data);
+      } catch (error) {
+        res.status(500).send(`Error: ${error.message}`);
+      }
+    });
+  } catch (error) {
+    console.log("DB error", error);
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log("server is running at port", port);
